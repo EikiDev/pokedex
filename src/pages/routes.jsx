6291eb3f92a57a44da7465af786261e5a723d3be
Lucas Eiki Gushiken <lushiken@gmail.com>
2024-12-19 21:48:54 -0300

    Browser Router para Hash Router

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import { HomePage } from "./home-page";
 import { PokemonPage } from "./pokemon-page";
 
 export const AppRoutes = () => (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route exact path="/pokedex" element={<HomePage />} />
             <Route exact path="/pokedex/pokemon/:id" element={<PokemonPage />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 );
